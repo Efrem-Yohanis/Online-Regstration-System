fbe62eea47dc86a65c265fc1e89a0243cfae7c01
Efrem-Yohanis-Anbese <efremyohanis116@gmail.com>
2024-01-19 17:44:21 +0300

    registration page

--- end_to_end/settings.py
+++ end_to_end/settings.py
@@ -32,12 +32,11 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
     ],
+    
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication'
-        ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
+        ]
+   
         
    
 }
