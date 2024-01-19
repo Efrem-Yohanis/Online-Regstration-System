4af8863a0284e2a2ff572ae4f939b08e2021bdc5
Efrem-Yohanis-Anbese <efremyohanis116@gmail.com>
2024-01-19 17:46:55 +0300

    registration page

--- api/views.py
+++ api/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.models import User
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated
-from rest_framework_simplejwt.tokens import RefreshToken
+
 
    
 @api_view(['GET'])
